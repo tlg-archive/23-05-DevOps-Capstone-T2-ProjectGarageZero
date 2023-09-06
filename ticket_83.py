490224ed052ec0494b249660113e4c705f672d11
crazyzoo143 <devcrazyzoo143@gmail.com>
2023-09-06 12:22:17 -0500

    choose new game AB#85

--- ticket_83.py
+++ ticket_83.py
@@ -37,3 +37,34 @@ def display_splash_screen():
 
     print("Player is presented with the option to play a New Game.")
     input("Press Enter to start a New Game...")
+
+def main_game_loop():
+    display_splash_screen()
+    while True:
+        clear_screen()
+        print("Welcome to Project Garage Zero: The Apprentice's Dilemma!")
+        print("1. Start a New Game")
+        print("2. Quit")
+        choice = input("> ")
+        if choice == '1':
+            # Start a new game
+            new_game()
+        elif choice == '2':
+            clear_screen()
+            print("Goodbye!")
+            break
+        else:
+            input("Invalid choice. Press Enter to continue...")
+
+def new_game():
+    clear_screen()
+    print("Game has started!")
+    # Game logic will go here
+    while True:
+        action = input("> ").strip()
+        if action.lower() == "quit":
+            break
+        # Handle game-specific commands and logic here
+
+if __name__ == "__main__":
+    main_game_loop()
