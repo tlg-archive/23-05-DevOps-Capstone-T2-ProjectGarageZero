1b8cdc93e6ca029af24d733d975d3eca19d08856
marylyn-rogel <marylyn.rogel1@gmail.com>
2023-09-12 16:09:36 -0500

    fixed the help screen and the welcome screen, AB#89

--- ticket_83.py
+++ ticket_83.py
@@ -18,25 +18,25 @@ def display_splash_screen():
     clear_screen()
 
     print("Story:")
-    print("You are trying to become an apprentice for MSM and you have just left your interview.")
+    print("\nYou are trying to become an apprentice for MSM and you have just left your interview.")
     print("But there's one problem: you can't remember where you parked your car!")
     print("You have a validated ticket but there's limited time before you have to pay extra for parking.")
     input("Press Enter to continue...")
     clear_screen()
 
     print("Objective:")
-    print("Before the step counter runs out, you need to find your car and leave the garage.")
+    print("\nBefore the step counter runs out, you need to find your car and leave the garage.")
     print("Face different obstacles on your quest to exit and avoid paying the additional parking fee.")
     input("Press Enter to continue...")
     clear_screen()
 
     print("Player:")
-    print("You are a young, bright-eyed individual, striving to become the next DevOps Engineer at MSM.")
+    print("\nYou are a young, bright-eyed individual, striving to become the next DevOps Engineer at MSM.")
     input("Press Enter to continue...")
     clear_screen()
 
     print("How to Win:")
-    print("Reach the garage exit and locate your car before the step counter runs out.")
+    print("\nReach the garage exit and locate your car before the step counter runs out.")
     input("Press Enter to continue...")
     clear_screen()
 
