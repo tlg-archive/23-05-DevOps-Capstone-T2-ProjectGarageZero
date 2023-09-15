e859322a3561c248d99a8c82f15f8948d033bcdc
ChicagoSam <sammytamimi@gmail.com>
2023-09-15 12:22:51 -0500

    polish splash before lock AB#83

--- ticket_83.py
+++ ticket_83.py
@@ -1,57 +1,49 @@
 import os
-#SAMMY: IMPORTING SYS FOR SYS.EXIT() TO QUIT
 import sys
-#SAMMY: CHANGING SO ONLY FUNCTION IMPORTED
+import textwrap
 from functionsTest import start_game
-#from functionsTest import *
 
 def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
 
+def print_centered(text, width=50):
+    wrapped_text = textwrap.fill(text, width=30)  # Setting width to 30 characters as per your request
+    for line in wrapped_text.split('\n'):
+        print(line.center(width))
+    print()  # Print an additional newline for spacing
+
 def display_splash_screen():
     clear_screen()
-    print("============================================")
-    print("          Project Garage Zero:               ")
-    print("       The Apprentice's Dilemma             ")
-    print("============================================")
-    input("Press Enter to continue...")
+    print_centered("==============================")
+    print_centered("Project Garage Zero:")
+    print_centered("The Apprentice's Dilemma")
+    print_centered("==============================")
+    input("\n\nPress Enter to continue...")
     clear_screen()
 
-    print("Story:")
-    print("\nYou are trying to become an apprentice for MSM and you have just left your interview.")
-    print("But there's one problem: you can't remember where you parked your car!")
-    print("You have a validated ticket but there's limited time before you have to pay extra for parking.")
-    input("Press Enter to continue...")
+    print_centered("STORY:")
+    print_centered("You are a young, bright-eyed individual, striving to become the next DevOps Engineer at MSM. You have just left your interview...")
+    print_centered("...and you can't remember where you parked your car!")
+    print_centered("You have a validated ticket but there's limited time before you have to pay extra for parking.")
+    input("\n\nPress Enter to continue...")
     clear_screen()
 
-    print("Objective:")
-    print("\nBefore the step counter runs out, you need to find your car and leave the garage.")
-    print("Face different obstacles on your quest to exit and avoid paying the additional parking fee.")
-    input("Press Enter to continue...")
+    print_centered("OBJECTIVE:")
+    print_centered("Find your car and leave the garage without paying an additional fee.")
+    print_centered("HOW TO WIN:")
+    print_centered("Locate your car and reach the garage exit before the step counter runs out.")
+    input("\n\nPress Enter to continue...")
     clear_screen()
-
-    print("Player:")
-    print("\nYou are a young, bright-eyed individual, striving to become the next DevOps Engineer at MSM.")
-    input("Press Enter to continue...")
-    clear_screen()
-
-    print("How to Win:")
-    print("\nReach the garage exit and locate your car before the step counter runs out.")
-    input("Press Enter to continue...")
-    clear_screen()
-
-    input("Press Enter to start a New Game...")
 
 def main_game_loop():
-    #display_splash_screen()
+    display_splash_screen()
     while True:
         clear_screen()
-        print("Welcome to Project Garage Zero: The Apprentice's Dilemma!")
+        print("Welcome to Project Garage Zero: The Apprentice's Dilemma!\n")
         print("1. Start a New Game")
         print("2. Quit")
-        choice = input("> ")
+        choice = input("\n> ")
         if choice == '1':
-            # SAMMY: CALLING FUNCTION I MADE FROM FUNCTIONSTEST
             start_game()
         elif choice == '2':
             clear_screen()
@@ -60,8 +52,5 @@ def main_game_loop():
         else:
             input("Invalid choice. Press Enter to continue...")
 
-
-
 if __name__ == "__main__":
     main_game_loop()
-
