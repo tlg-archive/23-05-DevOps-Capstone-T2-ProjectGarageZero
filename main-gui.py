6d26cbb46d78ca5a49787791fa713ec4ddf9b42f
Cayla Franklin <cjfranklin4@gmail.com>
2023-09-19 12:26:28 -0500

    displaying tkinter window, AB#692

---
+++ main-gui.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+
+class ProjectZeroGUI:
+    def __init__(self):
+
+        #initilize the tkinter window and size
+        self.root = tk.Tk()
+        self.root.geometry("500x500")
+
+        self.label.pack()
+        self.root.mainloop()
+
+    #define functions below
+
+if __name__ == "__main__":
+    ProjectZeroGUI()
