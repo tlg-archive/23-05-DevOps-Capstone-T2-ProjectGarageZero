a97d34b037e8c63900796fe3881dd2d0ffcbbaa8
Cayla Franklin <cjfranklin4@gmail.com>
2023-09-19 12:32:39 -0500

    added a button, AB#693

--- main-gui.py
+++ main-gui.py
@@ -6,8 +6,16 @@ class ProjectZeroGUI:
         #initilize the tkinter window and size
         self.root = tk.Tk()
         self.root.geometry("500x500")
+
+        #general label settings
         self.label = tk.Label(self.root, text="Hello World")
         self.label.pack()
+
+        #example button
+        self.start_button = tk.Button(self.root, text="Start Game")
+        self.start_button.pack()
+
+        #display the window
         self.root.mainloop()
 
     #define functions below
